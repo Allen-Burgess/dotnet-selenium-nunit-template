36aec468e5bcf889fea27bd3f5a76665dad4dfd8
Allen-Burgess <Allen2Burgess@gmail.com>
2020-03-03 16:14:38 +0000

    Added sample test data

--- Tests/SampleTests.cs
+++ Tests/SampleTests.cs
@@ -1,5 +1,7 @@
 ﻿using NUnit.Framework;
 using SeleniumBoilerplate.Config;
+using SeleniumBoilerplate.Data;
+using System.Collections.Generic;
 
 namespace SeleniumBoilerplate.Tests
 {
@@ -16,15 +18,36 @@ namespace SeleniumBoilerplate.Tests
 		/// Example test
 		/// </summary>
 		[Test]
-		public void SampleTest()
+		public void SampleTestOne()
 		{
 
 		}
+
+		/// <summary>
+		/// Example test with test data
+		/// </summary>
+		[TestCaseSource(nameof(SampleTestData))]
+		public void SampleTestTwo()
+		{
+
+		}
+
 
 		[TearDown]
 		public void TearDown()
 		{
 			TestCaseTearDown();
 		}
+
+		/// <summary>
+		/// Returns a testdata which behaves as test case of dictionaries  
+		/// </summary>
+		/// <returns> List of test cases</returns>
+		public static List<TestDataSet> SampleTestData()
+		{
+			// Enter the path to your excel sheet from the data folder
+			// no need to include extensions
+			return ExcelDataAccess.GetTestData("Sample/SampleData");
+		} 
 	}
 }
